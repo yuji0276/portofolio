4cf8a88d1bd15617192749417d537e55e1d7fcf9
yuji0276 <yujikami0110@gmail.com>
2026-07-27 11:25:03 +0900

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,24 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
+// ビルド時に生成する(Worker 側で動的に組み立てる必要がない)
+export const prerender = true;
+
 export async function GET(context) {
-	const posts = await getCollection("blog");
+	const posts = (await getCollection("blog", ({ data }) => !data.draft)).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+	);
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.pubDate,
+			categories: post.data.tags,
+			link: `/blogs/${post.id}/`,
 		})),
 	});
 }
